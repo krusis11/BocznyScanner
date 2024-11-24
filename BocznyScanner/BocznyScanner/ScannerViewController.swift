788b129df305fa9e651cf13283ea0597ab72519a
krusis11 <krusis.km@gmail.com>
2024-11-24 22:58:43 +0100

    dodane skanowanie

---
+++ BocznyScanner/BocznyScanner/ScannerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ScannerViewController.swift
+//  BocznyScanner
+//
+//  Created by Marcus on 24/11/2024.
+//
+
+import Foundation
---
+++ BocznyScanner/CodeDisplayView.swift
@@ -0,0 +1,8 @@
+//
+//  CodeDisplayView.swift
+//  BocznyScanner
+//
+//  Created by Marcus on 24/11/2024.
+//
+
+import Foundation
